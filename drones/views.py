00a4a751dad6dd50b9d0858a954e22b314027032
krom121 <stephenacgregor9@gmail.com>
2019-12-02 15:08:12 +0000

    endpoint

--- drones/views.py
+++ drones/views.py
@@ -58,3 +58,16 @@ class CompetitionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Competition.objects.all() 
     serializer_class = PilotCompetitionSerializer 
     name = 'competition-detail' 
+
+
+# API END POINT
+
+class ApiRoot(generics.GenericAPIView): 
+    name = 'api-root' 
+    def get(self, request, *args, **kwargs): 
+        return Response({ 
+            'drone-categories': reverse(DroneCategoryList.name, request=request), 
+            'drones': reverse(DroneList.name, request=request), 
+            'pilots': reverse(PilotList.name, request=request), 
+            'competitions': reverse(CompetitionList.name, request=request) 
+            }) 
