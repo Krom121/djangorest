281a4ec379c9edd3894830c00af0fed4014ca4a4
krom121 <stephenacgregor9@gmail.com>
2019-12-02 14:41:15 +0000

    complete serializers for drones app

--- drones/serializer.py
+++ drones/serializer.py
@@ -5,7 +5,15 @@ from drones.models import Pilot
 from drones.models import Competition 
 import drones.views 
  
- 
+"""
+The HyperlinkedModelSerializer is a type of ModelSerializer 
+that uses hyperlinked relationships instead of primary key 
+relationships, and therefore, it represents the relationships 
+to other model instances with hyperlinks instead of primary key values. 
+In addition, the HyperlinkedModelSerializer generates a field named url with the URL for the resource as its value. 
+As happens with ModelSerializer, the HyperlinkedModelSerializer class provides default implementations for the create and update methods.
+"""
+
 class DroneCategorySerializer(serializers.HyperlinkedModelSerializer): 
     drones = serializers.HyperlinkedRelatedField( 
         many=True, 
@@ -15,3 +23,41 @@ class DroneCategorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta: 
         model = DroneCategory 
         fields = ( 'url', 'pk', 'name', 'drones') 
+
+
+class DroneSerializer(serializers.HyperlinkedModelSerializer): 
+    # Display the category name 
+    drone_category = serializers.SlugRelatedField(queryset=DroneCategory.objects.all(), slug_field='name') 
+ 
+    class Meta: 
+        model = Drone 
+        fields = ( 'url', 'name', 'drone_category', 'manufacturing_date', 'has_it_competed', 'inserted_timestamp') 
+
+
+class CompetitionSerializer(serializers.HyperlinkedModelSerializer):
+    # Display all the details for the related drone
+    drone = DroneSerializer()
+    class Meta:
+        model = Competition
+        fields = ('url','pk','distance_in_feet','distance_achievement_date','drone')
+
+
+class PilotSerializer(serializers.HyperlinkedModelSerializer): 
+    competitions = CompetitionSerializer(many=True, read_only=True) 
+    gender = serializers.ChoiceField(choices=Pilot.GENDER_CHOICES) 
+    gender_description = serializers.CharField(source='get_gender_display',read_only=True) 
+ 
+    class Meta: 
+        model = Pilot 
+        fields = ('url','name','gender','gender_description','races_count','inserted_timestamp','competitions')
+
+
+ class PilotCompetitionSerializer(serializers.ModelSerializer): 
+    # Display the pilot's name 
+    pilot = serializers.SlugRelatedField(queryset=Pilot.objects.all(), slug_field='name') 
+    # Display the drone's name 
+    drone = serializers.SlugRelatedField(queryset=Drone.objects.all(), slug_field='name') 
+ 
+    class Meta: 
+        model = Competition 
+        fields = ('url','pk','distance_in_feet','distance_achievement_date','pilot','drone') 
