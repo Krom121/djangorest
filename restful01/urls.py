6dfcecf079d99ad26bfceabdaf8aac5d24f94fc5
krom121 <stephenacgregor9@gmail.com>
2019-12-03 14:01:15 +0000

    api-auth urls

--- restful01/urls.py
+++ restful01/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('toys.urls')),
     path('', include('drones.urls')),
+    path('api-auth/', include('rest_framework.urls'))
 ]
